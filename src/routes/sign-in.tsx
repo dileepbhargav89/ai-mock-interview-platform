87e66e9a248b7bf57f96e34f52fed441b3444626
Dileep Bhargav <dileepbhargav722@gmail.com>
2026-03-12 22:43:53 +0530

    Initial commit: AI Mock Interview Platform

---
+++ src/routes/sign-in.tsx
@@ -0,0 +1,14 @@
+import { SignIn } from "@clerk/clerk-react";
+
+export const SignInPage = () => {
+  return <SignIn path="/signin" />;
+};
+
+// import { SignIn } from "@clerk/clerk-react";
+
+// const SignInPage = () => {
+//   return <SignIn path="/signin" />
+  
+// };
+
+// export default SignInPage;
---
+++ src/routes/sign-up.tsx
@@ -0,0 +1,14 @@
+import { SignUp } from "@clerk/clerk-react";
+
+export const SignUpPage = () => {
+  return <SignUp path="/signup" />;
+};
+
+// import { SignUp } from "@clerk/clerk-react"
+
+// const SignUpPage = () => {
+//   return <SignUp path="/signup" />;
+
+// }
+
+// export default SignUpPage
